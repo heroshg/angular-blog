dca4d0eed0b2e3bfbbfaae7342e5075ace47fb03
heroshg <heros.goodoy@gmail.com>
2023-08-29 16:00:26 -0300

    alteração pessoal blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Comunicado de hoje(27/08): Neymar recebe péssima notícia no Al-Hilal e Jorge Jesus se revolta",
+    "description": "Neymar, sem sombra de dúvidas, é um dos assuntos mais comentados nas últimas semanas no futebol, principalmente, pelo contrato que fechou com o Al-Hilal. ",
+    "photoCover":"https://stcotvfoco.com.br/2023/08/neymar-e-jorge-jesus-juntos-e1693146608628.png"
   },
   {
     "id":"2",
-    "title": "Nova série anunciada no Disney+",
-    "description": "blabla bla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "VÍDEO: Neymar desembarca na Arábia Saudita e será apresentado neste sábado pelo Al-Hilal",
+    "description": "Neymar já está na Arábia Saudita. O craque brasileiro chegou ao país do Oriente Médio na noite desta Sexta-Feira(18)",
+    "photoCover":"https://imgs.search.brave.com/W1P0YIQbqbLWQ8vQdpGSd9Bsj4pGwO0dRL5Dg81Iu3M/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9wMi50/cnJzZi5jb20vaW1h/Z2UvZmdldC9jZi83/NzQvMC9pbWFnZXMu/dGVycmEuY29tLzIw/MjMvMDgvMTUvMTk2/MzY2MzQ2Mi1vZXNu/YmF4ZXRmYnZiZWl5/ZG1sN3hxcW9jdS5q/cGc"
+  },
+  {
+    "id":"3",
+    "title": "VÍDEO: Neymar é apresentado na Arábia Saudita com festa pirotécnica",
+    "description": "Neymar foi apresentado como reforço do Al-Hilal neste sábado(19), na Arábia Saudita, com uma festa pirotécnica no Estádio Rei Fahd.",
+    "photoCover":"https://imgs.search.brave.com/ruGRGgXtshnamNtEGtcMzBmmH4MBvUoRn-26b7eddCg/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9sbmNp/bWcubGFuY2UuY29t/LmJyL2Nkbi1jZ2kv/aW1hZ2Uvd2lkdGg9/MTkyMCxxdWFsaXR5/PTc1LGZvcm1hdD13/ZWJwL3VwbG9hZHMv/MjAyMy8wOC8wMDBf/MzNSTTZWTi5qcGc"
+  },
+  {
+    "id":"4",
+    "title": "'Ele quer escrever o capítulo que falta na carreira', comenta Diniz sobre o retorno de Neymar à Seleção",
+    "description": "O atacante Neymar, recém contratado pelo Al-Hilal da Arábia Saudita, voltou a ser convocado pela Seleção. O técnico Fernando Diniz convocou os 23 jogadores que vão representar o Brasil nas partidas.",
+    "photoCover":"https://imgs.search.brave.com/w-vuFh0rr1IEgbauPFPqp7c2YEBlw8v7pkMLczQFRqw/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTMy/NzE4MzYxNy9wdC9m/b3RvL25leW1hci1q/ci1vZi1icmF6aWwt/Y2VsZWJyYXRlcy1h/ZnRlci13aW5uaW5n/LWEtc2VtaS1maW5h/bC1tYXRjaC1vZi1j/b3BhLWFtZXJpY2Et/YnJhemlsLTIwMjEu/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PWxHdHVTOFRFZ3E3/c2xmWmpYUUhETWpI/d25keE0yRlNRTnRs/ZDE5aWt1ZUE9"
   }
+
 ]
